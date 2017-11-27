0b59a19c8d879944489a8222beb4514b05ec3431
solomongamid <sulimanabdulgadeir@gmail.com>
2017-11-27 13:30:00 +0100

    conflict

--- Carte.java
+++ Carte.java
@@ -9,32 +9,29 @@ public class Carte {
         char rocher = '1';
         for(int x = 0; x < 20; x++) {
             for(int y = 0; y < 20; y++) {
+
+                if(x==0 && y==0){
+                    board[x][y] = 'X';
+                }
                 
-                for (int i = 1; i <= 20; i++) { //20 As
-                     int randX = (int) (Math.random() * 20); //random number between 0-19
-                     int randY = (int) (Math.random() * 20);
-                     while (board[randX][randY] != null) { //find a free cell
-                         randX = (int) (Math.random() * 20);
-                         randY = (int) (Math.random() * 20);
-                      }
-                      board[randX][randY] = 'A'; //insert A
-                  }
-                board[0][0] = perssonage;
-                // for(int a = 0; a < 20; a++) {    
-                //     int chances = (int)(Math.random()*20)+1;      
-                //     if (chances == 1) {    
-                //         board[x][y] = 'A';
-                //     }
-                // }    
-                // for(int b = 0; b < 10; b++) {    
-                //     int chances = (int)(Math.random()*20)+1;      
-                //     if (chances == 1) {    
-                //         board[x][y] = 'B';
-                //     }
-                //     if(board[x][y] == 0 ) {
-                //         board[x][y] = 'C';
-                //     }     
-                // }        
+                for(int a = 0; a < 20; a++) {    
+                    int chances = (int)(Math.random()*20)+1;      
+                    if (chances == 1) {    
+                        board[x][y] = 'A';
+                    }
+                }    
+                for(int b = 0; b < 10; b++) {    
+                    int chances = (int)(Math.random()*20)+5;      
+                    if (chances == 1) {    
+                        board[x][y] = 'B';
+                    }     
+                }
+                for(int c = 0; c < 10; c++) {    
+                    int chances = (int)(Math.random()*20)+5;      
+                    if (chances == 1) {    
+                        board[x][y] = 'y';
+                    }     
+                }        
                 System.out.print(board[x][y]+"  ");
             }
 
